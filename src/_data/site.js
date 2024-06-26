259e73ce14498db4580f02dbb348fce7cc9171a0
Andrea Musso <eandram@hotmail.it>
2024-06-27 00:40:42 +0200

    first commit

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+	title: "Nucleus",
+	description:
+		"Nucleus is a venture studio focused on accelerating failure and perfecting execution. Not for everybody.",
+	// url: "https://eleventywebstarter.netlify.app",
+	author: "Andrea Musso",
+	env: process.env.ELEVENTY_ENV === "production",
+};
